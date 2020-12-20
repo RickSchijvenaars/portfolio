4b52cce70504d105257bb408cace9c3453d79e6d
RickSchijvenaars <36455350+RickSchijvenaars@users.noreply.github.com>
2020-12-20 20:12:30 +0100

    Added GA

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KRYLHKGFLM"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-KRYLHKGFLM');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Portfolio website of Rick Schijvenaars, a creative developer, who is passionate about creating beautiful digital products.">
